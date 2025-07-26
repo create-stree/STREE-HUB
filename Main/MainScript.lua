393443944a901388d54fbf06601ac7a116968733
create-stree <kirsiacb@gmail.com>
2025-07-27 00:02:01 +0900

    MainScript.lua

---
+++ Main/MainScript.lua
@@ -0,0 +1,6 @@
+-- Script utama yang akan dijalankan dari loader
+game.StarterGui:SetCore("SendNotification", {
+    Title = "STREE HUB",
+    Text = "Script berhasil dijalankan!",
+    Duration = 5
+})
