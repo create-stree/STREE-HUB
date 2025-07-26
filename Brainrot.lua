5d4c03c818c5fca9d6d62c47c16fc2d3ebdd3973
create-stree <kirsiacb@gmail.com>
2025-07-26 15:41:59 +0900

    Brainrot.lua

--- Brainrot.lua
+++ Brainrot.lua
@@ -1,7 +1,7 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({
-    Name = "STREE HUB | Steal A Brainrot | v0.15.25",
+    Name = "STREE HUB | Steal A Brainrot | v0.00.01",
     HidePremium = true,
     SaveConfig = true,
     ConfigFolder = "STREE HUB",
