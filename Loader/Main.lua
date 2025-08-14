cfed6345c40c15255f1bf90b18c1bafb82f757d5
create-stree <kirsiacb@gmail.com>
2025-08-14 19:55:40 +0900

    Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,1 +1,15 @@
+-- Multi-game loader
+local placeId = game.PlaceId
 
+if placeId == 1234567890 then
+    -- Game A
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-a-script/main.lua"))()
+elseif placeId == 9876543210 then
+    -- Game B
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-b-script/main.lua"))()
+elseif placeId == 2753915549 then
+    -- Game C
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-c-script/main.lua"))()
+else
+    warn("Game not supported!")
+end
