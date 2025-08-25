6b274066caa1d5466218e8d49e7dddf313de25b8
create-stree <kirsiacb@gmail.com>
2025-08-25 18:02:48 +0900

    Main.lua

--- Grow/Main.lua
+++ Grow/Main.lua
@@ -543,6 +543,7 @@ local function createSlider(text, min, max, default, callback)
       createToggleModern("Auto Sell", false, function(on)
            loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Grow/Auto%20sell.lua"))()
        end)
+
     contentFrame.CanvasSize = UDim2.new(0,0,yOffset,0)
 end)
 
