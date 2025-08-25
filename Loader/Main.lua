ff6954fe82c0854502c035bfefe699aa8fc49c67
create-stree <kirsiacb@gmail.com>
2025-08-25 18:16:44 +0900

    Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -5,8 +5,8 @@ if placeId == 79546208627805 then
     -- Game A
     loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-a-script/main.lua"))()
 elseif placeId == 126884695634066 then
-    -- Game B
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Grow/GrowAGarden.lua"))()
+    -- Game Grow A Garden
+    loadstring(game:HttpGet("h"))()
 elseif placeId == 2753915549 then
     -- Game C
     loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-c-script/main.lua"))()
