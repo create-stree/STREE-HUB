81a68e26f9a3ac2b045e1d756ee9117d4d941db8
create-stree <kirsiacb@gmail.com>
2025-09-04 12:13:56 +0900

    Update Test.lua

--- Loader/Test.lua
+++ Loader/Test.lua
@@ -11,7 +11,7 @@ end
 -- Buat Window
 local Window = WindUI:CreateWindow({
     Title = "STREE HUB",
-    Icon = "planet",
+    Icon = "Planet",
     Author = "KirsiaSC | Blox Fruit v0.00.01 | discord.gg/jdmX43t5mY",
     Folder = "STREE_HUB",
 })
