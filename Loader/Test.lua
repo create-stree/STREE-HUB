4a8ef4689a5e4df766cdf47e4374b16212ae9924
create-stree <kirsiacb@gmail.com>
2025-09-07 22:31:30 +0900

    Update Test.lua

--- Loader/Test.lua
+++ Loader/Test.lua
@@ -78,7 +78,7 @@ local Section = Tab1:Section({
     TextSize = 17,
 })
 
-local Paragraph = Tab1:Paragraph({
+local Paragraph1 = Tab1:Paragraph({
     Title = "Paragraph with Image, Thumbnail, Buttons",
     Desc = "Test Paragraph",
     Color = "Red",
