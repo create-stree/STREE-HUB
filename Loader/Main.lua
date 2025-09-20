2fcc68d48012a749abc05ffebc585a1a7e271197
create-stree <kirsiacb@gmail.com>
2025-09-20 09:50:33 +0900

    Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,18 +1,37 @@
--- Multi-game loader
 local placeId = game.PlaceId
+local StarterGui = game:GetService("StarterGui")
+local gameName, success = nil, false
 
 if placeId == 79546208627805 then
-    -- Game A
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Fish_It/Main.lua"))()
+    gameName = "Game A"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-a-script/main.lua"))()
+    success = true
 elseif placeId == 126884695634066 then
-    -- Game Grow A Garden
+    gameName = "Grow A Garden"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Loader/GrowAGarden.lua"))()
+    success = true
 elseif placeId == 2753915549 then
-    -- Game C
+    gameName = "Game C"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-c-script/main.lua"))()
+    success = true
 elseif placeId == 121864768012064 then
-    -- Fish It
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-c-script/main.lua"))()
+    gameName = "Fish It"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Fish_It/Main.lua"))()
+    success = true
+end
+
+if success and gameName then
+    StarterGui:SetCore("SendNotification", {
+        Title = "STREE HUB Loaded!",
+        Text = gameName .. " script loader!",
+        Duration = 6,
+        Icon = "rbxassetid://6023426926"
+    })
 else
-    warn("Game not supported!")
+    StarterGui:SetCore("SendNotification", {
+        Title = "STREE HUB Loaded!",
+        Text = "Scripts Not Found!",
+        Duration = 6,
+        Icon = "rbxassetid://6023426923"
+    })
 end
