d7acd65d7988beedc4967e0de1a6c30414070fda
create-stree <kirsiacb@gmail.com>
2025-09-20 23:34:36 +0900

    Main.lua

--- Forsaken/Main.lua
+++ Forsaken/Main.lua
@@ -5,6 +5,8 @@ end)
 if not success or not WindUI then
     warn("⚠️ UI failed to load!")
     return
+else
+    print("✓ UI loaded successfully!")
 end
 
 local Window = WindUI:CreateWindow({
@@ -19,18 +21,30 @@ local Window = WindUI:CreateWindow({
     HasOutline = true
 })
 
+Window:Tag({
+    Title = "v0.0.0.1",
+    Color = Color3.fromRGB(0, 255, 0),
+})
+
+WindUI:Notify({
+    Title = "STREE HUB Loaded",
+    Content = "UI loaded successfully!",
+    Duration = 3,
+    Icon = "bell",
+})
+
 local Tab1 = Window:Tab({
     Title = "Info",
     Icon = "info"
 })
 
-local Section = Tab1:Section({ 
+local InfoSection1 = Tab1:Section({ 
     Title = "Community Support",
     TextXAlignment = "Left",
     TextSize = 17,
 })
 
-Section1:Button({
+InfoSection1:Button({
     Title = "Discord",
     Desc = "click to copy link",
     Callback = function()
@@ -40,7 +54,7 @@ Section1:Button({
     end
 })
 
-local Section = Tab1:Section({ 
+local InfoSection2 = Tab1:Section({ 
     Title = "Every time there is a game update or someone reports something, I will fix it as soon as possible.",
     TextXAlignment = "Left",
     TextSize = 17,
@@ -51,13 +65,13 @@ local Tab2 = Window:Tab({
     Icon = "user"
 })
 
-local Section1 = Tab2:Section({
+local PlayerSection = Tab2:Section({
     Title = "Movement",
     TextXAlignment = "Left",
     TextSize = 17
 })
 
-Section1:Slider({
+PlayerSection:Slider({
     Title = "WalkSpeed",
     Description = "Adjust WalkSpeed",
     Default = 16,
@@ -69,7 +83,7 @@ Section1:Slider({
     end
 })
 
-Section1:Slider({
+PlayerSection:Slider({
     Title = "JumpPower",
     Description = "Adjust JumpPower",
     Default = 50,
@@ -96,13 +110,48 @@ local Tab5 = Window:Tab({
     Icon = "settings"
 })
 
-local Section2 = Tab5:Section({
-    Title = "System",
+local SettingSection = Tab5:Section({
+    Title = "Main",
     TextXAlignment = "Left",
     TextSize = 17
 })
 
-Section2:Toggle({
+SettingSection:Toggle({
+    Title = "AntiAFK",
+    Desc = "Prevent Roblox from kicking you when idle",
+    Icon = false,
+    Default = false,
+    Callback = function(state)
+        _G.AntiAFK = state
+        local VirtualUser = game:GetService("VirtualUser")
+        local player = game:GetService("Players").LocalPlayer
+        task.spawn(function()
+            while _G.AntiAFK do
+                task.wait(60)
+                pcall(function()
+                    VirtualUser:CaptureController()
+                    VirtualUser:ClickButton2(Vector2.new())
+                end)
+            end
+        end)
+        if state then
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "AntiAFK loaded!",
+                Text = "Coded By Kirsiasc",
+                Button1 = "Okey",
+                Duration = 5
+            })
+        else
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "AntiAFK Disabled",
+                Text = "Stopped AntiAFK",
+                Duration = 3
+            })
+        end
+    end
+})
+
+SettingSection:Toggle({
     Title = "Auto Reconnect",
     Desc = "Automatic reconnect if disconnected",
     Icon = false,
