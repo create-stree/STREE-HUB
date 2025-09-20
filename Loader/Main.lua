f38e51b66d1c58a8de0bd1680786df1f325dedc6
create-stree <kirsiacb@gmail.com>
2025-09-20 18:44:53 +0900

    Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -2,17 +2,17 @@ local placeId = game.PlaceId
 local StarterGui = game:GetService("StarterGui")
 local gameName, success = nil, false
 
-if placeId == 79546208627805 then
-    gameName = "Game A"
+if placeId == 2753915549 then
+    gameName = "Blox Fruit"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-a-script/main.lua"))()
     success = true
-elseif placeId == 126884695634066 then
-    gameName = "Grow A Garden"
+elseif placeId == 79546208627805 then
+    gameName = "99 Night In The Forest"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Loader/GrowAGarden.lua"))()
     success = true
-elseif placeId == 2753915549 then
-    gameName = "Game C"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-c-script/main.lua"))()
+elseif placeId == 18687417158 then
+    gameName = "Forsaken"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Forsaken/Main.lua"))()
     success = true
 elseif placeId == 121864768012064 then
     gameName = "Fish It"
