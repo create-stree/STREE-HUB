6bdb3ec47e4919a5f81d8e89bc9e6e7e07aee650
create-stree <kirsiacb@gmail.com>
2025-09-24 16:21:27 +0900

    Main.lua

--- Fish_It/Main.lua
+++ Fish_It/Main.lua
@@ -212,9 +212,59 @@ local Section = Tab3:Section({
     TextSize = 17,
 })
 
+local Tab3:Toggle({
+    Title = "Auto Fishing",
+    Desc = "Automatic fishing v1",
+    Icon = false,
+    Type = false,
+    Default = false,
+    Callback = function(value) 
+        _G.AutoFishing = value
+    end
+})
+
+local Players = game:GetService("Players")
+local RepStorage = game:GetService("ReplicatedStorage")
+local player = game.Players.LocalPlayer
+
+spawn(function()
+    while wait() do
+        if _G.AutoFishing then
+            repeat
+                  pcall(function()
+                       local char = player.Character or player.CharacterAdded:Wait()
+                       if char:FindFirstChild("!!!FISHING_VIEW_MODEL!!!") then
+                          RepStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/EquipToolFromHotbar"]:FireServer(1)
+                       end                    
+                       local cosmeticFolder = workspace:FindFirstChild("CosmeticFolder")
+                       if cosmeticFolder and not cosmeticFolder:FindFirstChild(tostring(player.UserId)) then
+                          RepStorage.Packages._Index["sleitnick_net@0.2.0"].net["RF/ChargeFishingRod"]:InvokeServer(2)
+                          wait(0.5)
+                          RepStorage.Packages._Index["sleitnick_net@0.2.0"].net["RF/RequestFishingMinigameStarted"]:InvokeServer(1, 1)
+                       end
+                  end)
+            wait(0.2)
+            until not _G.AutoFishing
+        end
+    end
+end)
+
+spawn(function()
+    while wait() do
+        if _G.AutoFishing then
+            repeat
+                  pcall(function()
+                       RepStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/FishingCompleted"]:FireServer()
+                  end)
+            wait(0.2)
+            until not _G.AutoFishing
+        end
+    end
+end)
+
 local Toggle = Tab3:Toggle({
     Title = "Auto fishing",
-    Desc = "Automatic fishing",
+    Desc = "Automatic fishing v2",
     Icon = false,
     Type = false,
     Default = false,
