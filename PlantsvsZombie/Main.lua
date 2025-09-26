5ad3bce6f8e5bbbf344e287b93c1e24000e7f75a
create-stree <kirsiacb@gmail.com>
2025-09-26 18:05:30 +0900

    Main.lua

--- PlantsvsZombie/Main.lua
+++ PlantsvsZombie/Main.lua
@@ -1,1 +1,90 @@
+local success, WindUI = pcall(function()
+    return loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+end)
 
+if not success or not WindUI then
+    warn("⚠️ UI failed to load!")
+    return
+else
+    print("✓ UI loaded successfully!")
+end
+
+local Window = WindUI:CreateWindow({
+    Title = "STREE HUB",
+    Icon = "rbxassetid://123032091977400",
+    Author = "KirsiaSC | Plants Vs Zombie",
+    Folder = "STREE_HUB",
+    Size = UDim2.fromOffset(560, 400),
+    Transparent = true,
+    Theme = "Dark",
+    SideBarWidth = 170,
+    HasOutline = true
+})
+
+Window:Tag({  
+    Title = "v0.0.0.1",  
+    Color = Color3.fromRGB(0, 255, 0),  
+})
+
+WindUI:Notify({  
+    Title = "STREE HUB Loaded",  
+    Content = "UI loaded successfully!",  
+    Duration = 3,  
+    Icon = "bell",  
+})
+
+local Tab1 = Window:Tab({
+    Title = "Info",
+    Icon = "info"
+})
+
+local Section = Tab1:Section({
+    Title = "Community Support",
+    TextXAlignment = "Left",
+    TextSize = 17
+})
+
+Tab1:Button({
+    Title = "Discord",
+    Desc = "Click to copy link",
+    Callback = function()
+        if setclipboard then
+            setclipboard("https://discord.gg/jdmX43t5mY")
+        end
+    end
+})
+
+Tab1:Button({
+    Title = "WhatsApp",
+    Desc = "Click to copy link",
+    Callback = function()
+        if setclipboard then
+            setclipboard("https://whatsapp.com/channel/0029VbAwRihKAwEtwyowt62N")
+        end
+    end
+})
+
+Tab1:Button({
+    Title = "Telegram",
+    Desc = "Click to copy link",
+    Callback = function()
+        if setclipboard then
+            setclipboard("https://t.me/StreeCoumminty")
+        end
+    end
+})
+
+Tab1:Button({
+    Title = "Website",
+    Desc = "Click to copy link",
+    Callback = function()
+        if setclipboard then
+            setclipboard("https://stree-hub-nexus.lovable.app")
+        end
+    end
+})
+
+local Tab2 = Window:Tab({
+    Title = "Main",
+    Icon = "landmark"
+})
