0a8b7321c82f4f9becd337d1b0912212523ea22c
create-stree <kirsiacb@gmail.com>
2025-09-26 22:57:31 +0900

    TestUI.lua

--- Loader/TestUI.lua
+++ Loader/TestUI.lua
@@ -1,6 +1,13 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
-local Window = WindUI:AddTheme({
+local Window = WindUI:CreateWindow({
+    Title = "STREE HUB",
+    Icon = "monitor",
+    Author = "KirsiaSC | Blox Fruit",
+    Folder = "STREE_HUB",
+    Size = UDim2.fromOffset(560, 400),
+    Transparent = true,
+    WindUI:AddTheme({
                Name="ChunkHub",
                Accent="#ffffff",
                Dialog="#ffffff",
@@ -10,4 +17,7 @@ local Window = WindUI:AddTheme({
                Background="#ffffff",
                Button="#090909",
                Icon="#090909",
-               })
+               }),
+    SideBarWidth = 170,
+    HasOutline = true
+})
