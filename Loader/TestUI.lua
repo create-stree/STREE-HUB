773603d9f009d977dce24e2d80eeabc679306d5c
create-stree <kirsiacb@gmail.com>
2025-09-26 22:49:28 +0900

    TestUI.lua

--- Loader/TestUI.lua
+++ Loader/TestUI.lua
@@ -1,39 +1,13 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
-local Window = WindUI:CreateWindow({
-    Title = "STREE HUB",
-    Icon = "rbxassetid://123032091977400",
-    Author = "KirsiaSC | Plants Vs Zombie",
-    Folder = "STREE_HUB",
-    Size = UDim2.fromOffset(560, 400),
-    Transparent = true,
-    SideBarWidth = 170,
-    Background = WindUI:Gradient({
-    ["0"] = { Color = Color3.fromHex("#0f0c29"), Transparency = 1 },
-    ["100"] = { Color = Color3.fromHex("#302b63"), Transparency = 0.9 },
-}, {
-    Rotation = 45,
-}),
-
-    KeySystem = { 
-        -- ↓ Optional. You can remove it.
-        Key = { "1234", "5678" },
-        
-        Note = "Example Key System.",
-        
-        -- ↓ Optional. You can remove it.
-        Thumbnail = {
-            Image = "rbxassetid://",
-            Title = "Thumbnail",
-        },
-        
-        -- ↓ Optional. You can remove it.
-        URL = "YOUR LINK TO GET KEY (Discord, Linkvertise, Pastebin, etc.)",
-        
-        -- ↓ Optional. You can remove it.
-        SaveKey = false, -- automatically save and load the key.
-        
-        -- ↓ Optional. You can remove it.
-        -- API = {} ← Services. Read about it below ↓
-    },
-})
+local Window = WindUI:AddTheme({
+               Name="ChunkHub",
+               Accent="#ffffff",
+               Dialog="#ffffff",
+               Outline="#5787d2",
+               Text="#090909",
+               Placeholder="#999999",
+               Background="#ffffff",
+               Button="#090909",
+               Icon="#090909",
+               })
