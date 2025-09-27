6e2c231fbf15b520d80fa33e2d199f0516a7c1b4
create-stree <kirsiacb@gmail.com>
2025-09-28 07:27:00 +0900

    TestUI.lua

--- Loader/TestUI.lua
+++ Loader/TestUI.lua
@@ -1,48 +1,22 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 WindUI:AddTheme({
-    Name = "Neon Green", 
+    Name = "JawaTheme",
     Accent = "#ffffff",
-    Dialog = "#1a1a1a",
-    Outline = "#ffffff",
-    Text = "#ffffff",
-    Placeholder = "#00cc00",
-    Background = "#39ff14",
-    Button = "#262626",
-    Icon = "#ffffff",
+    Dialog = "#ffffff",
+    Outline = "#5787d2",
+    Text = "#090909",
+    Placeholder = "#999999",
+    Background = "#ffffff",
+    Button = "#090909",
+    Icon = "#090909",
 })
 
-WindUI:SetNotificationLower(true)
-
-local themes = {"Neon Green"}
-local currentThemeIndex = 1
-
-getgenv().TransparencyEnabled = true
-
 local Window = WindUI:CreateWindow({
-    Title = "STREE HUB",
-    Icon = "rbxassetid://123032091977400", 
-    Author = "KirsiaSC | Plants vs Brainrot",
-    Folder = "STREE_HUB",
+    Title = "Jawa Hub",
+    Icon = "house",
+    Author = "Jawa",
+    Folder = "Jawa",
     Size = UDim2.fromOffset(560, 400),
-    Transparent = getgenv().TransparencyEnabled,
-    Theme = "Dark",
-    Resizable = true,
-    SideBarWidth = 150,
-    BackgroundImageTransparency = 0,
-    HideSearchBar = true,
-    ScrollBarEnabled = true,
-    User = {
-        Enabled = true,
-        Anonymous = false,
-        Callback = function()
-            currentThemeIndex = currentThemeIndex + 1
-            if currentThemeIndex > #themes then
-                currentThemeIndex = 1
-            end
-            
-            local newTheme = themes[currentThemeIndex]
-            WindUI:SetTheme(newTheme)
-        end,
-    },
+    Theme = "JawaTheme",
 })
