f19da45c047cab83098de3d4bc370af0dc65dc6b
create-stree <kirsiacb@gmail.com>
2025-09-27 15:09:21 +0900

    Main.lua

--- PlantsvsZombie/Main.lua
+++ PlantsvsZombie/Main.lua
@@ -19,6 +19,19 @@ local Window = WindUI:CreateWindow({
     Theme = "Dark",
     SideBarWidth = 170,
     HasOutline = true
+    User = {
+        Enabled = true,
+        Anonymous = false,
+        Callback = function()
+            currentThemeIndex = currentThemeIndex + 1
+            if currentThemeIndex > #themes then
+                currentThemeIndex = 1
+            end
+            
+            local newTheme = themes[currentThemeIndex]
+            WindUI:SetTheme(newTheme)
+        end,
+    },
 })
 
 Window:Tag({  
