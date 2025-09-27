0c51f8239cdf680cef94876325d151e966cc63cc
create-stree <kirsiacb@gmail.com>
2025-09-28 07:12:13 +0900

    Main.lua

--- Climb and Jump Tower/Main.lua
+++ Climb and Jump Tower/Main.lua
@@ -11,7 +11,7 @@ end
 
 local Window = WindUI:CreateWindow({
     Title = "STREE HUB",
-    Icon = "rbxassetid://123032091977400",
+    Icon = "rbxassetid://122683047852451",
     Author = "KirsiaSC | Climb and Jump Tower",
     Folder = "STREE_HUB",
     Size = UDim2.fromOffset(560, 400),
