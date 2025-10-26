1688a02ce3ab48a90412e19b919021bd4db055a7
create-stree <kirsiacb@gmail.com>
2025-10-26 10:18:58 +0900

    Test.lua

---
+++ Fish_It/Bruh.lua
@@ -0,0 +1,96 @@
+Tab3:Toggle({
+    Title = "Auto Fishing",
+    Desc = "Automatic Auto Fishing v1",
+    Icon = false,
+    Type = false,
+    Default = false,
+    Callback = function(value)
+        _G.AutoFishing = value
+    end
+})
+
+local RepStorage = game:GetService("ReplicatedStorage")
+
+spawn(function()
+    while wait() do
+        if _G.AutoFishing then
+            repeat
+                pcall(function()
+                    local char = player.Character or player.CharacterAdded:Wait()
+                    if char:FindFirstChild("!!!FISHING_VIEW_MODEL!!!") then
+                        RepStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/EquipToolFromHotbar"]:FireServer(1)
+                    end
+                    local cosmeticFolder = workspace:FindFirstChild("CosmeticFolder")
+                    if cosmeticFolder and not cosmeticFolder:FindFirstChild(tostring(player.UserId)) then
+                        RepStorage.Packages._Index["sleitnick_net@0.2.0"].net["RF/ChargeFishingRod"]:InvokeServer(2)
+                        wait(0.5)
+                        RepStorage.Packages._Index["sleitnick_net@0.2.0"].net["RF/RequestFishingMinigameStarted"]:InvokeServer(1,1)
+                    end
+                end)
+                wait(0.2)
+            until not _G.AutoFishing
+        end
+    end
+end)
+
+spawn(function()
+    while wait() do
+        if _G.AutoFishing then
+            repeat
+                pcall(function()
+                    RepStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/FishingCompleted"]:FireServer()
+                end)
+                wait(0.2)
+            until not _G.AutoFishing
+        end
+    end
+end)
+
+local RunService = game:GetService("RunService")    
+local Workspace = game:GetService("Workspace")    
+local VirtualInputManager = game:GetService("VirtualInputManager")    
+local ReplicatedStorage = game:GetService("ReplicatedStorage")    
+local camera = Workspace.CurrentCamera    
+    
+local REEquipToolFromHotbar = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/EquipToolFromHotbar"]    
+local REFishingCompleted = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/FishingCompleted"]
+
+local autoHoldEnabled = false
+Tab3:Toggle({
+    Title = "Auto Fishing",
+    Desc = "Automatic Fishing v2",
+    Value = false,
+    Callback = function(state)
+        autoHoldEnabled = state
+        if state then
+            WindUI:Notify({
+                Title = "Auto Fishing V2",
+                Content = "Enabled",
+                Duration = 3
+            })
+            task.spawn(function()
+                local holdDuration = 0.4
+                local loopDelay = 0.2
+                while autoHoldEnabled do
+                    pcall(function()
+                        REEquipToolFromHotbar:FireServer(1)
+                        local clickX = 5
+                        local clickY = camera.ViewportSize.Y - 5
+                        VirtualInputManager:SendMouseButtonEvent(clickX, clickY, 0, true, game, 0)
+                        task.wait(holdDuration)
+                        VirtualInputManager:SendMouseButtonEvent(clickX, clickY, 0, false, game, 0)
+                    end)
+                    task.wait(loopDelay)
+                    RunService.Heartbeat:Wait()
+                end
+            end)
+        else
+            WindUI:Notify({
+                Title = "Auto Fishing V2",
+                Content = "Disabled",
+                Duration = 3
+            })
+        end
+    end
+})
+
