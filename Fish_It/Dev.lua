190216aba156b6c23bdc1bc4f00bb33f27282531
create-stree <kirsiacb@gmail.com>
2025-10-27 19:08:18 +0900

    Update Dev.lua

--- Fish_It/Dev.lua
+++ Fish_It/Dev.lua
@@ -370,42 +370,39 @@ local VirtualInputManager = game:GetService("VirtualInputManager")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")    
 local camera = Workspace.CurrentCamera    
     
-local REEquipToolFromHotbar = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/EquipToolFromHotbar"]    
-local REFishingCompleted = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/FishingCompleted"]
+local REEquipToolFromHotbar = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/EquipToolFromHotbar"]
 
 local autoHoldEnabled = false
+
 Toggle = Tab3:Toggle({
-    Title = "Auto Fishing",
-    Desc = "Automatic Auto Fishing v2",
+    Title = "Legit",
+    Desc = "Automatic Legit Fishng",
     Value = false,
     Callback = function(state)
         autoHoldEnabled = state
         if state then
             WindUI:Notify({
-                Title = "Auto Fishing V2",
-                Content = "Enabled",
+                Title = " AutoLegit",
+                Content = "🚀 Extreme Speed Enabled",
                 Duration = 3
             })
+
             task.spawn(function()
-                local holdDuration = 0.4
-                local loopDelay = 0.2
                 while autoHoldEnabled do
                     pcall(function()
                         REEquipToolFromHotbar:FireServer(1)
                         local clickX = 5
                         local clickY = camera.ViewportSize.Y - 5
                         VirtualInputManager:SendMouseButtonEvent(clickX, clickY, 0, true, game, 0)
-                        task.wait(holdDuration)
                         VirtualInputManager:SendMouseButtonEvent(clickX, clickY, 0, false, game, 0)
                     end)
-                    task.wait(loopDelay)
                     RunService.Heartbeat:Wait()
                 end
             end)
         else
             WindUI:Notify({
-                Title = "Auto Fishing V2",
-                Content = "Disabled",
+                Title = "Auto Legit",
+                Content = "❌ Disabled",
                 Duration = 3
             })
         end
@@ -437,66 +434,7 @@ local Toggle = Tab3:Toggle({
         end)    
     end    
 })    
-    
-local Section = Tab3:Section({     
-    Title = "Other",    
-    TextXAlignment = "Left",    
-    TextSize = 17,    
-})
 
-local autoInstantFishEnabled = true
-local delayTime = 0.1
-
-local function startAutoFish()
-    task.spawn(function()
-        while autoInstantFishEnabled do
-            pcall(function()
-                REFishingCompleted:FireServer()
-            end)
-            task.wait(delayTime)
-        end
-    end)
-end
-
-Toggle = Tab3:Toggle({
-    Title = "Auto Instant complete Fishing",
-    Desc = "Instant Fishing For v2 (It is mandatory to turn it on if you want to use Auto Fishing V2)",
-    Value = autoInstantFishEnabled,
-    Callback = function(state)
-        autoInstantFishEnabled = state
-        if state then
-            WindUI:Notify({
-                Title = "Auto Instant Fish",
-                Content = "Enabled (Delay: " .. delayTime .. "s)",
-                Duration = 3
-            })
-            startAutoFish()
-        else
-            WindUI:Notify({
-                Title = "Auto Instant Fish",
-                Content = "Disabled",
-                Duration = 3
-            })
-        end
-    end
-})
-    
-local Toggle = Tab3:Toggle({    
-    Title = "Instant Catch",    
-    Desc = "Get fish straight away",    
-    Icon = false,    
-    Type = false,    
-    Default = false,    
-    Callback = function(state)    
-        _G.InstantCatch = state    
-        if state then    
-            print("✅ Instant Catch ON")    
-        else    
-            print("❌ Instant Catch OFF")    
-        end    
-    end    
-})    
-    
 local Toggle = Tab3:Toggle({    
     Title = "Radar",    
     Desc = "Toggle fishing radar",    
