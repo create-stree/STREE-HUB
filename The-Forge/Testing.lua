fbe33e387231a036a3eb1dac7b3efab5e1d23eb7
create-stree <kirsiacb@gmail.com>
2025-12-12 20:02:12 +0900

    Update Testing.lua

--- The-Forge/Testing.lua
+++ The-Forge/Testing.lua
@@ -74,7 +74,7 @@ G2L["ButtonRezise_2"].BorderSizePixel = 0
 G2L["ButtonRezise_2"].Draggable = true
 G2L["ButtonRezise_2"].BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 G2L["ButtonRezise_2"].Image = "rbxassetid://123032091977400"
-G2L["ButtonRezise_2"].Size = UDim2.new(0, 53, 0, 53)
+G2L["ButtonRezise_2"].Size = UDim2.new(0, 50, 0, 50)
 G2L["ButtonRezise_2"].Position = UDim2.new(0.13, 0, 0.03, 0)
 
 local corner = Instance.new("UICorner", G2L["ButtonRezise_2"])
