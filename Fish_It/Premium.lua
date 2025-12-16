1df5a9eefff64e4045c2eb422b4233d3831142b8
create-stree <kirsiacb@gmail.com>
2025-12-16 12:37:24 +0900

    Update Premium.lua

--- Fish_It/Premium.lua
+++ Fish_It/Premium.lua
@@ -77,7 +77,7 @@ G2L["ButtonRezise_2"].Visible = false
 G2L["ButtonRezise_2"].Visible = false
 
 Window:Tag({
-    Title = "v0.0.2.7",
+    Title = "v0.0.2.8",
     Color = Color3.fromRGB(0, 255, 0),
     Radius = 17,
 })
@@ -2626,3 +2626,4 @@ Tab7:Button({
         loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))()
     end
 })
+
