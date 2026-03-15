56b7193a4541cbbcbccf5d9e5f1a7ddcf0c4cc96
create-stree <kirsiacb@gmail.com>
2026-03-15 23:12:41 +0900

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -11,27 +11,31 @@ local streeLogo = "rbxassetid://99948086845842"
 local gameScripts = {
     [127794225497302] = {
         name = "Abyss",
-        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Abyss/Premium.lua"
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Abyss/Main.lua"
+    },
+    [127794225497302] = {
+        name = "Arsenal",
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Arsenal/Main.lua"
     },
     [124311897657957] = {
         name = "Break A Lucky Block",
-        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/BALB/Premium.lua"
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/BALB/Main.lua"
     },
     [2753915549] = {
         name = "Blox Fruit",
-        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/main/Premium.lua"
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/main/Main.lua"
     },
     [123921593837160] = {
         name = "Climb and Jump Tower",
-        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Climb%20and%20Jump%20Tower/Premium.lua"
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Climb%20and%20Jump%20Tower/Main.lua"
     },
     [131623223084840] = {
         name = "Escape Tsunami For Brainrot",
-        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/ETFB/Premium.lua"
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/ETFB/Main.lua"
     },
     [121864768012064] = {
         name = "Fish It",
-        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Fish_It/Premium.lua"
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Fish_It/Main.lua"
     },
     [18687417158] = {
         name = "Forsaken",
@@ -39,11 +43,11 @@ local gameScripts = {
     },
     [130594398886540] = {
         name = "Garden Horizons",
-        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Garden-Horizons/Premium.lua"
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Garden-Horizons/Main.lua"
     },
     [136599248168660] = {
         name = "Solo Hunter",
-        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Solo-Hunter/Premium.lua"
+        free = "https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Solo-Hunter/Main.lua"
     }
 }
 
